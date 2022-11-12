44bd20b990f5d33c428a65b4a474b9c839d7b958
ErikRBjorklund <erbjorklund@icloud.com>
2022-11-11 19:09:23 -0500

    Consistent Movement

--- myfirst.js
+++ myfirst.js
@@ -8,7 +8,7 @@ var uid = 1;
 wss.on("connection", ws => {
   console.log("New client has connected!");
 
-  user.set(uid, [0, 0])
+  user.set(uid, [0, 0, "", ""]) //x y U/D(keyup/keydown) K(previous key)
   
   ws.send(`PUID ${uid}`);
   uid++;
@@ -19,8 +19,8 @@ wss.on("connection", ws => {
     if (!data.includes("CONNECTED")) {
       console.log(hold[0]);
       console.log(user.get(parseInt(hold[0])));
-      const tid = parseInt(hold[0]);
-      handle_input(tid, hold[1]);
+      
+      handle_input(hold);
       
     }
   })
@@ -28,8 +28,8 @@ wss.on("connection", ws => {
   ws.on("close", () => {
     console.log("Client has disconnected!");
   })
-
-  const interval = setInterval(send_data, 50);
+  const interval2 = setInterval(do_movement, 20);
+  const interval1 = setInterval(send_data, 20);
   function send_data() {
     for (let [id, pos] of user) {
       ws.send(`UID ${id} X ${pos[0]} Y ${pos[1]}`);
@@ -39,20 +39,41 @@ wss.on("connection", ws => {
 
 
 
-function handle_input(tid, input) {
-  if (input.toString() ==="W") {
-    user.get(tid)[1] = user.get(tid)[1] - 5;
-  } else if (input.toString() ==="A") {
-    user.get(tid)[0] = user.get(tid)[0] - 5;
-  } else if (input.toString() ==="S") {
-    user.get(tid)[1] = user.get(tid)[1] + 5;
-  } else if (input.toString() ==="D") {
-    user.get(tid)[0] = user.get(tid)[0] + 5;
-  }
-
-  
+function handle_input(input) {
+  const tid = parseInt(input[0]);
+  user.set(tid, [user.get(tid)[0], user.get(tid)[1], input[1], input[2]]);
 }
 
+// function handle_input(tid, input) {
+//   if (input.toString() === "W") {
+//     user.get(tid)[1] = user.get(tid)[1] - 5;
+//   } else if (input.toString() === "A") {
+//     user.get(tid)[0] = user.get(tid)[0] - 5;
+//   } else if (input.toString() === "S") {
+//     user.get(tid)[1] = user.get(tid)[1] + 5;
+//   } else if (input.toString() === "D") {
+//     user.get(tid)[0] = user.get(tid)[0] + 5;
+//   }
+// }
+
+function do_movement() {
+  for (let [id, pos] of user) {
+    if (pos[2] === "D") {
+      if (pos[3] == "W") {
+        user.set(id, [pos[0], pos[1] - 5, pos[2], pos[3]])
+      }
+      else if (pos[3] == "A") {
+        user.set(id, [pos[0] - 5, pos[1], pos[2], pos[3]])
+      }
+      else if (pos[3] == "S") {
+        user.set(id, [pos[0], pos[1] + 5, pos[2], pos[3]])
+      }
+      else if (pos[3] == "D") {
+        user.set(id, [pos[0] + 5, pos[1], pos[2], pos[3]])
+      }
+    }
+  }
+}
 
 
 
