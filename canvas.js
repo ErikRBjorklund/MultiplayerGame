bde4ec063fe63458b5555aa5ecd987a5203c6626
ErikRBjorklund <erbjorklund@icloud.com>
2022-11-11 22:03:50 -0500

    Test Push

--- canvas.js
+++ canvas.js
@@ -13,11 +13,19 @@ player.src = './player.png';
 
 class Sprite {
     constructor({position, velocity, image}) {
+        console.log("hi");
         this.position = position
         this.image = image
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        this.rgb = `rgb(${r},${g},${b})`;
     }
     draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
+        // c.drawImage(this.image, this.position.x, this.position.y)
+        c.fillStyle = this.rgb;
+        c.fillRect(this.position.x, this.position.y, 50, 50);
     }
 }
 
@@ -40,20 +48,35 @@ function animate() {
 }
 
 function update_pos(x, y, tid) {
-    const player_sprite = new Sprite({
-        position: {
-            x: x,
-            y: y
-        },
-        image: player
-    })
-    playerMap.set(tid, player_sprite)
+    if (playerMap.has(tid)) {
+        playerMap.get(tid).position.x = x;
+        playerMap.get(tid).position.y = y;
+    } else {
+        const player_sprite = new Sprite({
+            position: {
+                x: x,
+                y: y
+            },
+            image: player
+        })
+        playerMap.set(tid, player_sprite)
+    }
+    
+    
 }
 
 function draw_users() {
     for (let x of playerMap) {
         x[1].draw();
     }
+}
+
+function random_rgba() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    return `rgb(${r},${g},${b})`;
 }
 
 animate();
